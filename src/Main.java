66c2a8fab33194bf0cb844242792024ad4721933
RenitaT <renitat2025@student.cis.edu.hk>
2023-01-31 13:01:48 +0800

    completed tutorial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,68 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //1
+        String myFavouriteArtist = "Toby Fox";
+
+        //2
+        System.out.println(myFavouriteArtist);
+
+        //3
+        int myFavouriteArtistLength = myFavouriteArtist.length();
+
+        //4
+        System.out.println(myFavouriteArtist.length());
+
+        //5
+        String myFavouriteArtistUpper = myFavouriteArtist.toUpperCase();
+
+        //6
+        System.out.println(myFavouriteArtistUpper);
+
+        //7
+        String myFavouriteArtistLower = myFavouriteArtist.toLowerCase();
+
+        //8
+        System.out.println(myFavouriteArtistLower);
+
+        //9
+        String checkFor = "ift";
+
+        //10
+        boolean myFavouriteAristContains = myFavouriteArtist.contains(checkFor);
+
+        //11
+        System.out.println(myFavouriteAristContains);
+
+        //12
+        String firstName = "Renita";
+
+        //13
+        String lastName = "Tang";
+
+        //14
+        String age = "16";
+
+        //15
+        String myInfo;
+
+        //16
+        myInfo = "First Name: "+firstName+"\nLast Name: "+lastName+"\nAge: "+age+"\nFavourite Artist: "+myFavouriteArtist+"";
+
+        //17
+        System.out.println(myInfo);
+
+        //18
+        String stringToIntTest  = "16";
+
+        //19
+        int stringToIntTestStore = Integer.parseInt(stringToIntTest);
+
+        //20
+        String stringToStringTest = "" + stringToIntTest;
+
+
+
+
     }
 }
